173fbc58cffa81a5290d5584d4316bf5b90d8afe
Nati_Shchiglik <natishch94@gmail.com>
2019-11-02 13:43:22 +0200

    commit

--- Keywords.py
+++ Keywords.py
@@ -3,6 +3,7 @@ import Classes
 import copy
 import xlwt
 
+
 def ret_courses_from_sheet_one():
     result_data = []
     file_name = 'Workbook1.xlsx'
@@ -18,7 +19,6 @@ def ret_courses_from_sheet_one():
                 data = sheet.cell_value(curr_row, curr_col)  # Read the data in the current cell
                 row_data.append(data)
             result_data.append(row_data)
-
     return parse_courses_names(result_data)
 
 
@@ -41,10 +41,11 @@ def ret_schedule_of_teacher(sheet):
     result = []
     num_rows = 10  # Number of Rows
     num_cols = 5  # Number of Columns
+
     for curr_col in range(0, num_cols):
         for curr_rows in range(0, num_rows):
             data = sheet.cell_value(curr_rows, curr_col)  # Read the data in the current cell
-            result.append((int)(data))
+            result.append(int(data))
     return result
 
 
@@ -80,34 +81,41 @@ def find_courses_obj_by_str(Courses, c_name):
                 break
     return c
 
+
 def ret_total_hours(Course):
     total = 0
     for i in Course:
         total = total + i.hoursPerWeek
     return total
 
-def createExelTableForStudents(Curses, Teacher, solotion):
+
+def createExelTableForStudents(Curses, Teacher, solution):
     fileName = 'Schedule_Students.xls'
     excel_file = xlwt.Workbook()
     sheet1 = excel_file.add_sheet('first year')
     sheet2 = excel_file.add_sheet('second year')
     sheet3 = excel_file.add_sheet('third year')
-    for i in solotion:
-        if(Curses[i[2]].year == 1):
-            sheet1.write(i[1]%10,int(i[1]/10),Teacher[i[0]].name + " : " + Curses[i[2]].name)
-        elif(Curses[i[2]].year == 2):
+
+    for i in solution:
+        if Curses[i[2]].year == 1:
+            sheet1.write(i[1] % 10, int(i[1]/10), Teacher[i[0]].name + " : " + Curses[i[2]].name)
+        elif Curses[i[2]].year == 2:
             sheet2.write(i[1] % 10, int(i[1] / 10), Teacher[i[0]].name + " : " + Curses[i[2]].name)
         else:
             sheet3.write(i[1] % 10, int(i[1] / 10), Teacher[i[0]].name + " : " + Curses[i[2]].name)
 
     excel_file.save(fileName)
 
-def createExelTableForteachers(Curses, Teacher, solotion):
+
+def createExelTableForteachers(Curses, Teacher, solution):
     fileName = 'Schedule_Teachers.xls'
     sheetsArray = []
     excel_file = xlwt.Workbook()
+
     for i in Teacher:
         sheetsArray.append(excel_file.add_sheet(i.name))
-    for s in solotion:
-        sheetsArray[s[0]].write(s[1]%10, int(s[1]/10), Curses[s[2]].name)
+
+    for s in solution:
+        sheetsArray[s[0]].write(s[1] % 10, int(s[1]/10), Curses[s[2]].name)
     excel_file.save(fileName)
+
--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 import Keywords
 from docplex.mp.model import Model
 
-from decimal import Decimal
-
 Courses = Keywords.ret_courses_from_sheet_one()
 Teachers = Keywords.ret_teachers_list(Courses)
 
-th = Keywords.ret_total_hours(Courses)
-X = [(i,j,k.index) for i in range(len(Teachers)) for j in range(50) for k in Teachers[i].courses]
+X = [(i, j, k.index) for i in range(len(Teachers)) for j in range(50) for k in Teachers[i].courses]
+
 mdl = Model('AN')
-A = mdl.binary_var_dict(X,name='A')
-mdl.minimize(mdl.sum(mdl.sum(mdl.sum(A[i,j,k.index]*(int)(Teachers[i].schedule[j])*Teachers[i].seniority for k in Teachers[i].courses)for j in range(50))for i in range(len(Teachers))))
+A = mdl.binary_var_dict(X, name='A')
+
+mdl.minimize(mdl.sum(mdl.sum(mdl.sum(A[i, j, k.index]*int(Teachers[i].schedule[j])*Teachers[i].seniority for k in Teachers[i].courses)for j in range(50))for i in range(len(Teachers))))
+
 for i in range(len(Teachers)):
     for j in range(50):
-        mdl.add_constraint(mdl.sum(A[i,j,k.index] for k in Teachers[i].courses)<=1)
-# for i in range(len(Teachers)):
-#     for j in range(50):
-#         mdl.add_constraint(mdl.sum(A[i, j, k.index] for k in Teachers[i].courses) == Courses[k.index].hoursPerWeek)
-# mdl.add_constraint(mdl.sum(mdl.sum(mdl.sum(A[i, j, k.index] for k in Teachers[i].courses)for j in range(50))for i in range(len(Teachers))) == Courses[k.index].hoursPerWeek)
+        mdl.add_constraint(mdl.sum(A[i, j, k.index] for k in Teachers[i].courses) <= 1)
+
 for i in range(len(Teachers)):
     for k in Teachers[i].courses:
         print(k.name)
         mdl.add_constraint(mdl.sum(A[i, j, k.index] for j in range(50)) == Courses[k.index].hoursPerWeek)
+
 for j in range(50):
     mdl.add_constraint(mdl.sum(mdl.sum(A[i, j, k.index] for k in Teachers[i].courses if k.year == 1)for i in range(len(Teachers))) <= 1)
     mdl.add_constraint(mdl.sum(mdl.sum(A[i, j, k.index] for k in Teachers[i].courses if k.year == 2)for i in range(len(Teachers))) <= 1)
@@ -30,11 +28,11 @@ for j in range(50):
 for i in range(len(Teachers)):
     for d in range(5):
         for k in Teachers[i].courses:
-            mdl.add_constraint(mdl.sum(A[i, (d*10+h), k.index]+A[i, (d*10+h+1), k.index] for h in range(9)) <= 2);
-
+            mdl.add_constraint(mdl.sum(A[i, (d*10+h), k.index]+A[i, (d*10+h+1), k.index] for h in range(9)) <= 2)
 
 solution = mdl.solve(log_output=True)
 sol = [a for a in X if A[a].solution_value == 1.0]
-print(sol)
+
 Keywords.createExelTableForStudents(Courses, Teachers, sol)
 Keywords.createExelTableForteachers(Courses, Teachers, sol)
+
